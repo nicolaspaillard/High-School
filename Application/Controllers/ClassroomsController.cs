7b0010faa8679a38a966eb25092b01a64c769f1c
MaxR <maxime.ramadanoski@gmail.com>
2021-06-17 11:35:21 +0200

    Rollback Packages to 5.0.6 CREATE ALL CRUDs

---
+++ Application/Controllers/ClassroomsController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Dal;
+using Models;
+
+namespace Application.Controllers
+{
+    public class ClassroomsController : Controller
+    {
+        private readonly HighSchoolContext _context;
+
+        public ClassroomsController(HighSchoolContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Classrooms
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Classrooms.ToListAsync());
+        }
+
+        // GET: Classrooms/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var classroom = await _context.Classrooms
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (classroom == null)
+            {
+                return NotFound();
+            }
+
+            return View(classroom);
+        }
+
+        // GET: Classrooms/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Classrooms/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,Name")] Classroom classroom)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(classroom);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(classroom);
+        }
+
+        // GET: Classrooms/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var classroom = await _context.Classrooms.FindAsync(id);
+            if (classroom == null)
+            {
+                return NotFound();
+            }
+            return View(classroom);
+        }
+
+        // POST: Classrooms/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Name")] Classroom classroom)
+        {
+            if (id != classroom.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(classroom);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ClassroomExists(classroom.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(classroom);
+        }
+
+        // GET: Classrooms/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var classroom = await _context.Classrooms
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (classroom == null)
+            {
+                return NotFound();
+            }
+
+            return View(classroom);
+        }
+
+        // POST: Classrooms/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var classroom = await _context.Classrooms.FindAsync(id);
+            _context.Classrooms.Remove(classroom);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ClassroomExists(int id)
+        {
+            return _context.Classrooms.Any(e => e.ID == id);
+        }
+    }
+}
