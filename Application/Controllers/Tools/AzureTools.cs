1eb9454bdc0f893978ca65ef53378bd44246ef0e
Formation <Formation@DESKTOP-MM7JUH8>
2021-06-22 15:27:44 +0200

    La joie

---
+++ Application/Controllers/Tools/AzureTools.cs
@@ -0,0 +1,22 @@
+﻿using Application.Repositories.IRepositories;
+using Microsoft.Identity.Web;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Application.Controllers.Tools
+{
+    public static class AzureTools
+    {
+        public static IEnumerable<string> RegisterAzureUser(ClaimsPrincipal user)
+        {
+            //var currentId = user.Claims.FirstOrDefault(c => c.Type == ClaimConstants.ObjectId.ToString()).Value;
+            //var role = user.Claims.FirstOrDefault(c => c.Type == ClaimConstants.Role.ToString()).Value;
+            var roles = ((ClaimsIdentity)user.Identity).Claims.Where(c => c.Type == ClaimTypes.Role).Select(c => c.Value).ToList();
+            return roles;        
+        }
+    }
+}
